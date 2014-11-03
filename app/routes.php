e5b38cded44577f67b71aedb4af0afec4eba5475
alex <als128@uakron.edu>
2014-11-03 09:53:03 -0500

    hook test

--- app/routes.php
+++ app/routes.php
@@ -38,6 +38,7 @@ Route::any('/bitbucket/{token}', function($token){
         $commits = Input::get('commits');
         $doPull = false;
 
+
         foreach($commits as $commit){
             if($commit['branch'] == 'dev') {
                 $doPull = true;
