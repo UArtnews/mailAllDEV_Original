5b37247af60a8f2e2de9ba8a70d75daa9d956203
alex <als128@uakron.edu>
2014-11-03 09:54:29 -0500

    tweak to log file

--- app/routes.php
+++ app/routes.php
@@ -33,7 +33,7 @@ Route::any('/bitbucket/{token}', function($token){
     foreach(Input::get('payload') as $name => $value){
         $vars .= $name. '<br/>';
     }
-    File::put('/web_content/share/mailAllSource/log.json', $vars);
+    File::put('/web_content/share/mailAllSource/log.json', Input::get('payload'));
     if(Input::has('commits') && $token == '5237239250'){
         $commits = Input::get('commits');
         $doPull = false;
