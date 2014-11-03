0d9bf9ef707c605d9b18fa12608022f61c9955ca
alex <als128@uakron.edu>
2014-11-03 10:36:47 -0500

    Actual  Hook  Test

--- app/routes.php
+++ app/routes.php
@@ -50,6 +50,7 @@ Route::any('/bitbucket/{token}', function($token){
                 $doPull = true;
             }
         }
+
         
         if($doPull) {
             $log .= "Doing Pull!\n";
