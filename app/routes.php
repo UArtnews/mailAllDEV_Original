ea6b1870027919f62882c9563bbab17385209006
alex <als128@uakron.edu>
2014-11-03 09:56:20 -0500

    hook test

--- app/routes.php
+++ app/routes.php
@@ -33,7 +33,6 @@ Route::any('/bitbucket/{token}', function($token){
     foreach(Input::get('payload') as $name => $value){
         $vars .= $name. '<br/>';
     }
-
     File::put('/web_content/share/mailAllSource/log.json', json_encode(Input::get('payload')));
 
     if(Input::has('commits') && $token == '5237239250'){
