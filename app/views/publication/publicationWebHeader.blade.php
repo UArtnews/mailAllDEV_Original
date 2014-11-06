9698e227120afb1747bdc8864455446a19796597
alex <als128@uakron.edu>
2014-11-06 14:54:27 -0500

    LAST STUFF

---
+++ app/views/publication/publicationWebHeader.blade.php
@@ -0,0 +1,3 @@
+@if(!$isEmail)
+{{ isset($tweakables['publication-web-header']) ? str_replace('**DATE**',date('F j, Y', strtotime($publication->publish_date)), $tweakables['publication-web-header']) : str_replace('**DATE**',date('F j, Y', strtotime($publication->publish_date)), $default_tweakables['publication-web-header']) }}
+@endif
