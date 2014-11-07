dc5b6b0968df2947b5bcd323d4f845e3cd639926
alex <als128@uakron.edu>
2014-11-07 08:50:58 -0500

    Initial Permissions

--- app/routes.php
+++ app/routes.php
@@ -553,6 +553,7 @@ Route::group(array('before' => 'force.ssl'), function(){
 //                              //
 //////////////////////////////////
 Route::group(array('before' => 'force.ssl'), function(){
+    dd(Request::server('uanetID'));
     //Editor Routing
     Route::get('/edit/{instanceName}/{action?}/{subAction?}', function($instanceName, $action = null, $subAction = null) {
         $app = app();
