212741103c8c72244881517bb70d3d5d12d9b50d
alex <als128@uakron.edu>
2015-02-17 15:07:23 -0500

    Dump test

--- app/controllers/EmailController.php
+++ app/controllers/EmailController.php
@@ -91,6 +91,19 @@ class EmailController extends \BaseController
     }
 
     public function mergeEmail($instanceName, $publication_id){
+
+        Mail::send('html',array('html' => $mergedHTML),function ($message) use($addressTo) {
+            $message->to($addressTo)
+                ->subject(Input::has('subject') ? stripslashes(Input::get('subject')) : '')
+                ->from(Input::get('addressFrom'), Input::has('nameFrom') ? Input::get('nameFrom') : '')
+                ->replyTo(Input::get('replyTo'), Input::has('nameFrom') ? Input::get('nameFrom') : '');
+        });
+        echo "Subject: ".Input::has('subject');
+        echo "<br/>From Address: ".Input::get('addressFrom');
+        echo "<br/>From Name: ".Input::get('nameFrom');
+        echo "<br/>ReplyTo Address: ".Input::get('replyTo');
+        die;
+
         set_time_limit(600);
 //        ini_set('memory_limit','320M');
         $instance = Instance::where('name', $instanceName)->first();
