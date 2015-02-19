6e4a355f436149917670c12bd49ca61f79b62f6c
alex <als128@uakron.edu>
2015-02-19 10:54:03 -0500

    Optimized class loader

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
     |
     */
 
-    'url'       => 'http://localhost',
+    'url'       => 'http://share.uakron.edu/mailAll',
 
     /*
     |--------------------------------------------------------------------------
