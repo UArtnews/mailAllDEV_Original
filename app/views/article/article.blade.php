834ab8d021c571b7dbe854249a3758d92b1b4305
alex <als128@uakron.edu>
2015-03-20 09:34:08 -0400

    Read More article snippets

--- app/views/article/article.blade.php
+++ app/views/article/article.blade.php
@@ -10,9 +10,8 @@
     @elseif($isRepeat && $hideRepeat && !$isEditable)
         <a name="articleTitle{{ $article->id }}"></a>
         <a href="{{ preg_replace('/https/','http', URL::to($instanceName.'/archive/'.$article->originalPublication().'#article'.$article->id), 1) }}">
-            <h1 id="articleTitle{{ $article->id }}" class="articleTitle{{ $isEditable ? ' editable' : '' }}">{{ $article->getTitle() }}
-                <br/><small>[Click to read more]</small>
-            </h1>
+            <h1 id="articleTitle{{ $article->id }}" class="articleTitle{{ $isEditable ? ' editable' : '' }}">{{ $article->getTitle() }}</h1>
+            <h1><small>[Click to read more]</small></h1>
         </a>
     {{-- Setup Href Headlines for Emails --}}
     @elseif($isEmail)
